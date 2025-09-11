5f1c9168b9f0ba004d6eecc6349f726f9c5a76db
Jim Jin <gjin@bu.edu>
2025-09-11 11:21:05 -0400

    added views.py

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+#create view here
+
+def home(request):
+'''Fund to respond to the "home" request.'''
+
+response_text = '''
+<html>
+<h1>Hello, world!</h1>
+</html> 
+'''
+
+return HttpResponse(response_text) 
+
+
+
+
