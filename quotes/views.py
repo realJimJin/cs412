d1f0a0530315570933e79c4a12077992858c7c90
Jim Jin <gjin@bu.edu>
2025-09-11 11:27:56 -0400

    fixed bugs in views.py

--- quotes/views.py
+++ quotes/views.py
@@ -4,15 +4,15 @@ from django.http import HttpRequest, HttpResponse
 #create view here
 
 def home(request):
-'''Fund to respond to the "home" request.'''
+    '''Fund to respond to the "home" request.'''
 
-response_text = '''
-<html>
-<h1>Hello, world!</h1>
-</html> 
-'''
+    response_text = '''
+    <html>
+    <h1>Hello, world!</h1>
+    </html> 
+    '''
 
-return HttpResponse(response_text) 
+    return HttpResponse(response_text) 
 
 
 
