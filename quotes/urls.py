5f169043ca149b8740311032d8f0cf7070618e14
Jim Jin <gjin@bu.edu>
2025-09-11 11:16:58 -0400

    created quotes/urls.py

---
+++ quotes/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path 
+from django.conf import settings 
+from . import views 
+
+#URL patterns specific to the quotes app: 
+
+urlpatterns = [
+ path(r'', views.home, name="home"),
+]
