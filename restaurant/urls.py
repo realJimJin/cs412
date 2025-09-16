21ec3b8a1fb918438d2bef8e9586b131de67a294
Jim Jin <gjin@bu.edu>
2025-09-16 11:42:57 -0400

    added url structure for restaurant

---
+++ restaurant/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from django.conf import settings 
+from . import views
+
+#URL patterns specific to the restaurant app:
+
+urlpatterns = [
+ path(r'', views.home, name="home"),
+ path(r'main', views.main, name="main_page"),
+path(r'order', views.order, name="order_page"),
+path(r'confirmation', views.confirmation, name="confirmation_page"),
+]
+
