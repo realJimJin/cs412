aefad78c31d5fcb53239366bda05152a96d8ec88
Jim Jin <gjin@bu.edu>
2025-09-25 12:10:22 -0400

    models.py

---
+++ mini_insta/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+# Create your models here.
+class (models.Model):
+    '''Encapsulate the data of a user profile on mini_insta.'''
+
+    # define the data attributes of the Article object
+    username = models.TextField(blank=True)
+    display_name = models.TextField(blank=True)
+    bio_text = models.TextField(blank=True) 
+    join_date = models.DateTimeField(auto_now=True)
+    profile_image_url = models.URLField(blank=True) # url as a string
+    #user = models.ForeignKey(User, on_delete=models.CASCADE) 
+ def __str__(self):
+        '''return a string representation of this model instance.'''
+        return f'{self.username} displays as {self.display_name}'
+    
