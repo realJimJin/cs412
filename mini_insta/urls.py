2712e21e411b64b2bc88798e8618c0df575b5e3d
Jim Jin <gjin@bu.edu>
2025-09-26 19:23:56 -0400

    typo

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-path('', ShowAllView.as_view(), name="show_all"), 
+path('', ProfileListView.as_view(), name="show_all_profiles"), 
 path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"), 
 ]
