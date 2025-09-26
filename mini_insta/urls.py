c4447e461cbfbb150bdb3366fbbd2ce3639096c8
Jim Jin <gjin@bu.edu>
2025-09-26 19:21:01 -0400

    modified urls.py

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
+path('', ShowAllView.as_view(), name="show_all"), 
 path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"), 
 ]
