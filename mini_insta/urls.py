bfe3c408a81f52546ae48fba90bd48878f80e0f4
Jim Jin <gjin@bu.edu>
2025-09-26 20:35:25 -0400

    app name

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from .views import * 
 
+app_name = "mini_insta" 
+
 urlpatterns = [
 path('', ProfileListView.as_view(), name="show_all_profiles"), 
 path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"),
