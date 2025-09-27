e6b3a04265635d31527215b6615e5cfe9c24d3ec
Jim Jin <gjin@bu.edu>
2025-09-26 20:18:17 -0400

    changed views.py

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
 from .models import Profile 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class ProfileListView(ListView):
@@ -11,12 +11,10 @@ class ProfileListView(ListView):
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
 
-    def dispatch(self, request, *args, **kwargs):
-        '''Override the dispatch method to add debugging information.'''
 
-        if request.user.is_authenticated:
-            print(f'ProfileListView.dispatch(): request.user={request.user}')
-        else:
-            print(f'ProfileListView.dispatch(): not logged in.')
+class ProfileView(DetailView):
+    '''Display a single profile.'''
 
-        return super().dispatch(request, *args, **kwargs)
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile" # note singular variable name
