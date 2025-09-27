92a3bb34c44c2947ef3184476337566e57564e1b
Jim Jin <gjin@bu.edu>
2025-09-26 20:24:05 -0400

    urls.py

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
 path('', ProfileListView.as_view(), name="show_all_profiles"), 
-path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"), 
+path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"),
+path('profile/<int:pk>', ProfileDetailView.as_view(), name="show_profile"), 
 ]
