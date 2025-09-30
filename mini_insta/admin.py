fd72554084de7179f21e2bc40403fae66b1193e3
Jim Jin <gjin@bu.edu>
2025-09-30 12:08:55 -0400

    updated admin

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Profile
+from .models import Post
+from .models import Photo
+
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Photo) 
