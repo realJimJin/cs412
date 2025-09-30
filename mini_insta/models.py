75e5e5b93ab4a63c2026949fe00b627fba04941e
Jim Jin <gjin@bu.edu>
2025-09-30 12:02:01 -0400

    updated Photo model

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -35,9 +35,9 @@ class Photo(models.Model):
 
     # data attributes for the Photo:
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    caption = models.TextField(blank=False)
+    image_url = models.TextField(blank=False)
     timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         '''Return a string representation of this Photo.'''
-        return f'{self.caption}'    
+        return f'{self.image_url}'    
