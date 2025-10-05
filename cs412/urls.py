51b818b56fcea0cc16b187794e4ef2eb7cc43687
Jim Jin <gjin@bu.edu>
2025-10-05 17:00:43 -0400

    undo

--- cs412/urls.py
+++ cs412/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('quotes/', include ('quotes.urls') ),
     path('restaurant/', include ('restaurant.urls') ),
-    path('mini_insta/', include ('mini_insta.urls'), namespace="mini_insta" ),  
+    path('mini_insta/', include ('mini_insta.urls')),  
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
