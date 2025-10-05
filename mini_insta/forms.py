1ece1c59fd19b11fdc8f7d40c6fbbab98b0e0f71
Jim Jin <gjin@bu.edu>
2025-10-05 16:18:19 -0400

    added forms.py

---
+++ mini_insta/forms.py
@@ -0,0 +1,13 @@
+# mini_insta/forms.py
+# define the forms that we use for create/update/delete operations
+
+from django import forms
+from .models import Profile, Post
+
+class CreatePostForm(forms.ModelForm):
+    '''A form to add a post to the database.'''
+
+    class Meta:
+        '''associate this form with a model from our database.'''
+        model = Post
+        fields = ['profile', 'caption', 'time_stamp']
