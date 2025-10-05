c6b2722220934e31d885c946069dd0ab43f10228
Jim Jin <gjin@bu.edu>
2025-10-05 11:25:19 -0400

    edited urls.py

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -7,5 +7,6 @@ app_name = "mini_insta"
 urlpatterns = [
 path('', ProfileListView.as_view(), name="show_all_profiles"), 
 path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"),
-path('profile/<int:pk>', ProfileDetailView.as_view(), name="show_profile"), 
+path('profile/<int:pk>', ProfileDetailView.as_view(), name="show_profile"),
+path('post/<int:pk>', PostDetailView.as_view(), name="show_post"),  
 ]
