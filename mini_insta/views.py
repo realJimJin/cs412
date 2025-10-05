91c2b489ba6b7e870efce09aee3995bcfd0aaac8
Jim Jin <gjin@bu.edu>
2025-10-05 16:33:41 -0400

    updated views.py

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -25,4 +25,36 @@ class PostDetailView(DetailView):
 
     model = Post
     template_name = "mini_insta/show_post.html"
-    context_object_name = "post" # note singular variable name 
+    context_object_name = "post" # note singular variable name
+
+
+class CreatePostView(CreateView):
+    '''A view to handle creation of a new Post on a profile.'''
+
+    form_class = CreatePostForm
+    template_name = "mini_insta/create_post_form.html"
+
+    def get_success_url(self):
+        '''Provide a URL to redirect to after creating a new Post.'''
+
+        # create and return a URL:
+        # return reverse('show_all') # not ideal; we will return to this
+        # retrieve the PK from the URL pattern
+        pk = self.kwargs['pk']
+        # call reverse to generate the URL for this Article
+        return reverse('profile', kwargs={'pk':pk})
+
+    def get_context_data(self):
+        '''Return the dictionary of context variables for use in the template.'''
+
+        # calling the superclass method
+        context = super().get_context_data()
+
+        # find/add the profile to the context data
+        # retrieve the PK from the URL pattern
+        pk = self.kwargs['pk']
+        profile = Profile.objects.get(pk=pk)
+
+        # add this profile into the context dictionary:
+        context['profile'] = profile
+        return context 
