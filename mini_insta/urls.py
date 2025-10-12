7cea71302de3dee75a35850353d76dc72215a573
Jim Jin <gjin@bu.edu>
2025-10-12 18:40:35 -0400

    updated urls.py

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,5 +1,7 @@
 
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import * 
 
 app_name = "mini_insta" 
@@ -11,3 +13,6 @@ path('profile/<int:pk>', ProfileDetailView.as_view(), name="show_profile"),
 path('post/<int:pk>', PostDetailView.as_view(), name="show_post"),
 path('profile/<int:pk>/create_post', CreatePostView.as_view(), name="create_post"),  
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
