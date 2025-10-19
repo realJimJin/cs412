4dc1cbe459cc3f7a504dfb7b994142286e3ba538
Jim Jin <gjin@bu.edu>
2025-10-19 19:56:35 -0400

    added to admin

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -6,8 +6,10 @@ from .models import Profile
 from .models import Post
 from .models import Photo
 from .models import Follow
+from .models import Comment 
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Photo)
-admin.site.register(Follow)  
+admin.site.register(Follow) 
+admin.site.register(Comment) 
