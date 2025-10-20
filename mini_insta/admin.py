d2d1a2b5a08acc3f847775d1d625d62662ba5ada
Jim Jin <gjin@bu.edu>
2025-10-19 20:15:38 -0400

    added Like in admin

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -6,10 +6,12 @@ from .models import Profile
 from .models import Post
 from .models import Photo
 from .models import Follow
-from .models import Comment 
+from .models import Comment
+from .models import Like  
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Photo)
 admin.site.register(Follow) 
-admin.site.register(Comment) 
+admin.site.register(Comment)
+admin.site.register(Like) 
