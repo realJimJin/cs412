628aa0df107d8085a986c8bddb527860ebce33fa
Jim Jin <gjin@bu.edu>
2025-10-26 19:32:39 -0400

    added import

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.db.models import Prefetch
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile (models.Model):
