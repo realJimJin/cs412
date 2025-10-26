3fa720f5d646c7a4ecb1ebdbb59542bfced5d6de
Jim Jin <gjin@bu.edu>
2025-10-26 19:50:20 -0400

    new views

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,94 +1,126 @@
-
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render, redirect
-from .models import Profile, Post, Photo 
-from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
-from .forms import CreatePostForm, PhotoForm, UpdateProfileForm, UpdatePostForm 
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
-from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.db.models import Q
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+
+from .models import Profile, Post, Photo
+from .forms import CreatePostForm, PhotoForm, UpdateProfileForm, UpdatePostForm
+
+# --- Small helper mixins so views can find "my" Profile and enforce ownership ---
+
+class MustBeLoggedIn(LoginRequiredMixin):
+    login_url = "mini_insta:login"
+
+    def get_current_profile(self):
+        """Return the Profile belonging to the logged-in User."""
+        return get_object_or_404(Profile, user=self.request.user)
+
+
+class MustOwnPost(MustBeLoggedIn):
+    """Ensure the current user owns the Post being edited/deleted."""
+    def dispatch(self, request, *args, **kwargs):
+        resp = super().dispatch(request, *args, **kwargs)
+        post = getattr(self, "object", None) or self.get_object()
+        if post.profile.user != request.user:
+            from django.http import HttpResponseForbidden
+            return HttpResponseForbidden("You don't have permission for this post.")
+        return resp
+
+
+# ---------------- Public (read-only) views ----------------
+
 class ProfileListView(ListView):
-    '''Define a view class to show all instagram profiles.'''
-    
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
 
 
 class ProfileDetailView(DetailView):
-    '''Display a single profile.'''
-
     model = Profile
     template_name = "mini_insta/show_profile.html"
-    context_object_name = "profile" # note singular variable name
+    context_object_name = "profile"
 
 
 class PostDetailView(DetailView):
-    '''Display a single profile.'''
-
     model = Post
     template_name = "mini_insta/show_post.html"
-    context_object_name = "post" # note singular variable name
+    context_object_name = "post"
 
 
+# ---------------- Owned views (require login, no pk in URL) ----------------
 
-class CreatePostView(CreateView):
+class MyProfileDetailView(MustBeLoggedIn, DetailView):
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile"
+
+    def get_object(self):
+        return self.get_current_profile()
+
+
+class CreatePostView(MustBeLoggedIn, CreateView):
     form_class = CreatePostForm
     template_name = "mini_insta/create_post_form.html"
 
-    def get_success_url(self):
-        return reverse('mini_insta:show_profile', kwargs={'pk': self.kwargs['pk']})
-
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
-        ctx['profile'] = profile
-        # provide photo_form in both GET and POST
-        if self.request.method == "POST":
-            ctx['photo_form'] = PhotoForm(self.request.POST, self.request.FILES)
-        else:
-            ctx['photo_form'] = PhotoForm()
+        ctx["profile"] = self.get_current_profile()
+        ctx["photo_form"] = PhotoForm(self.request.POST or None, self.request.FILES or None)
         return ctx
 
     def post(self, request, *args, **kwargs):
-        """Process both the Post form and the Photo form together."""
         self.object = None
-        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        profile = self.get_current_profile()
 
-        form = self.get_form()  # CreatePostForm bound to POST
+        form = self.get_form()
         photo_form = PhotoForm(request.POST, request.FILES)
 
         if form.is_valid() and photo_form.is_valid():
-            # Save Post
             post = form.save(commit=False)
             post.profile = profile
             post.save()
 
-            # Save Photo only if a file was provided
-            if photo_form.cleaned_data.get('image_file'):
+            if photo_form.cleaned_data.get("image_file"):
                 photo = photo_form.save(commit=False)
                 photo.post = post
                 photo.save()
 
-            return redirect(self.get_success_url())
+            return redirect(reverse("mini_insta:show_post", kwargs={"pk": post.pk}))
 
-        # Re-render with errors and the missing context vars
         return render(request, self.template_name, {
-            'form': form,
-            'photo_form': photo_form,
-            'profile': profile,
+            "form": form,
+            "photo_form": photo_form,
+            "profile": profile,
         })
-   
-class UpdateProfileView(UpdateView):
+
+
+class UpdateProfileView(MustBeLoggedIn, UpdateView):
     model = Profile
     form_class = UpdateProfileForm
     template_name = "mini_insta/update_profile_form.html"
-    # No need to define get_success_url if Profile.get_absolute_url() exists
+
+    def get_object(self):
+        return self.get_current_profile()
 
 
-class DeletePostView(DeleteView):
+class UpdatePostView(MustOwnPost, UpdateView):
+    model = Post
+    form_class = UpdatePostForm
+    template_name = "mini_insta/update_post_form.html"
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["profile"] = self.get_object().profile
+        return ctx
+
+    def get_success_url(self):
+        return reverse("mini_insta:show_post", kwargs={"pk": self.get_object().pk})
+
+
+class DeletePostView(MustOwnPost, DeleteView):
     model = Post
     template_name = "mini_insta/delete_post_form.html"
     context_object_name = "post"
@@ -101,87 +133,58 @@ class DeletePostView(DeleteView):
         return ctx
 
     def get_success_url(self):
-        post = self.get_object()
-        return reverse("mini_insta:show_profile", kwargs={"pk": post.profile.pk})
+        return reverse("mini_insta:my_profile")
 
 
-class UpdatePostView(UpdateView):
-    model = Post
-    form_class = UpdatePostForm
-    template_name = "mini_insta/update_post_form.html"
-    context_object_name = "post"
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx["profile"] = self.get_object().profile
-        return ctx
-
-    def get_success_url(self):
-        post = self.get_object()
-        return reverse("mini_insta:show_post", kwargs={"pk": post.pk})
-
-class ShowFollowersDetailView(DetailView):
-    """Display a Profile and the list of Profiles who follow it."""
-    model = Profile
-    template_name = "mini_insta/show_followers.html"
-    context_object_name = "profile"  # template gets `profile`
-
-class ShowFollowingDetailView(DetailView):
-    """Display a Profile and the list of Profiles this Profile follows."""
-    model = Profile
-    template_name = "mini_insta/show_following.html"
-    context_object_name = "profile"
-
-class PostFeedListView(ListView):
-    """Feed of posts from profiles that this profile follows."""
+class PostFeedListView(MustBeLoggedIn, ListView):
     template_name = "mini_insta/show_feed.html"
     context_object_name = "posts"
-    paginate_by = 10  # optional: add pagination
-
-    def dispatch(self, request, *args, **kwargs):
-        self.profile = get_object_or_404(Profile, pk=self.kwargs["pk"])
-        return super().dispatch(request, *args, **kwargs)
+    paginate_by = 10
 
     def get_queryset(self):
-        return self.profile.get_post_feed()
+        me = self.get_current_profile()
+        # assumes you implemented Profile.get_post_feed() that returns a QS
+        return me.get_post_feed().order_by("-timestamp").select_related("profile")
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx["profile"] = self.profile
+        ctx["profile"] = self.get_current_profile()
         return ctx
 
-class SearchView(ListView):
-    """Search Posts (by caption) and Profiles (by username/display_name/bio)."""
+
+class SearchView(MustBeLoggedIn, ListView):
     template_name = "mini_insta/search_results.html"
-    context_object_name = "posts"  # the ListView queryset name
+    context_object_name = "posts"
 
     def dispatch(self, request, *args, **kwargs):
-        # who is doing the search
-        self.profile = get_object_or_404(Profile, pk=self.kwargs["pk"])
+        self.me = self.get_current_profile()
         self.query = request.GET.get("q")
-        # If no query yet, show the search form page
         if not self.query:
-            return render(request, "mini_insta/search.html", {"profile": self.profile})
-        # Otherwise proceed to results
+            return render(request, "mini_insta/search.html", {"profile": self.me})
         return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        # Return matching Posts when a query is present
-        return (
-            Post.objects
-            .filter(caption__icontains=self.query)
-            .select_related("profile")
-            .order_by("-timestamp")
-        )
+        return (Post.objects
+                .filter(caption__icontains=self.query)
+                .select_related("profile")
+                .order_by("-timestamp"))
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx["profile"] = self.profile
+        ctx["profile"] = self.me
         ctx["query"] = self.query or ""
-        # Profiles that match (username, display_name, or bio_text)
         ctx["profile_results"] = Profile.objects.filter(
             Q(username__icontains=self.query) |
             Q(display_name__icontains=self.query) |
             Q(bio_text__icontains=self.query)
         ).order_by("display_name")
         return ctx
+
+
+# ---------------- Auth views (templates: login.html) ----------------
+
+class LoginView(auth_views.LoginView):
+    template_name = "mini_insta/login.html"
+
+class LogoutView(auth_views.LogoutView):
+    next_page = "mini_insta:show_all_profiles"
