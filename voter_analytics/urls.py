3bde857d68d3b667910121c9b2da7c8588f276cd
Jim Jin <gjin@bu.edu>
2025-11-02 15:09:25 -0500

    urls.py

---
+++ voter_analytics/urls.py
@@ -0,0 +1,8 @@
+# voter_analytics/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.VoterListView.as_view(), name='voters'),               # ''
+    path('voter/<int:pk>/', views.VoterDetailView.as_view(), name='voter') # 'voter/<int:pk>'
+]
