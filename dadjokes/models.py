346d79f354e00f07d960484e6989149a24d18363
Jim Jin <gjin@bu.edu>
2025-11-16 17:43:02 -0500

    modified models

---
+++ dadjokes/models.py
@@ -0,0 +1,21 @@
+
+# Create your models here.
+
+from django.db import models
+
+class Joke(models.Model):
+    text = models.TextField()
+    contributor = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.text[:40]}... by {self.contributor}'
+
+
+class Picture(models.Model):
+    image_url = models.URLField()
+    contributor = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Picture by {self.contributor}'
