9cc5a5601500cfb71e253c1be27193f9d32787cf
Jim Jin <gjin@bu.edu>
2025-11-16 20:08:19 -0500

    fixed files

--- cs412/urls.py
+++ cs412/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('restaurant/', include ('restaurant.urls') ),
     path('mini_insta/', include ('mini_insta.urls')),
     path('voter_analytics/', include('voter_analytics.urls')),
-    path('yourprefix/dadjokes/', include('dadjokes.urls')),  
+    path('dadjokes/', include('dadjokes.urls')),  
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
